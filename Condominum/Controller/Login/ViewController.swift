9fa389d397e606f373fee7426fc51f7d9ad8c27c
BelenG <allison_jb@hotmail.com>
2019-10-14 22:14:36 -0500

    login3

--- Condominum/Controller/Login/ViewController.swift
+++ Condominum/Controller/Login/ViewController.swift
@@ -12,24 +12,28 @@ class ViewController: UIViewController {
  
     
     @IBOutlet weak var header: UIImageView!
+    
     @IBOutlet weak var logo: UIImageView!
+    
     @IBOutlet weak var loginDataView: UIView!
     @IBOutlet weak var LoginDataLineView: UIView!
+    
     @IBOutlet weak var userTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var initSessionButton: UIButton!
+    
     @IBOutlet weak var forgottenPasswordButton: UIButton!
     
-    private let primaryColor = UIColor(red: 56/255, green: 117/255, blue: 233/255, alpha: 1)
-    private let secondaryColor = UIColor(red: 173/255, green: 202/255, blue: 250/255, alpha: 1)
+    private let primaryColor = UIColor(red: 197/255, green: 111/255, blue: 243/255, alpha: 1)
+    private let secondaryColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
     private let tertiaryColor = UIColor(red: 254/255, green: 254/255, blue: 254/255, alpha: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        logo.layer.cornerRadius = logo.bounds.height / 2
-        logo.clipsToBounds = true
+        //logo.layer.cornerRadius = logo.bounds.height / 2
+        //logo.clipsToBounds = true
         
         loginDataView.layer.borderColor = tertiaryColor.cgColor
         loginDataView.layer.borderWidth = 1
@@ -46,13 +50,26 @@ class ViewController: UIViewController {
     @IBAction func btnSessionClick(_ sender: Any) {
         let user = userTextField.text!
         let pass = passwordTextField.text!
-        let login = RequestLogin(Username : user , Password: pass)
+        
+        let login = RequestLogin(user: user, pass: pass)
+        
+        print("asdf")
         
         ApiService.login(request: login, completion : { data in
-            let alert = UIAlertController(title: "Inicio de sesion", message: "bienvendio " + data.firstName + " " + data.lastName, preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "Entiendo", style: UIAlertAction.Style.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+            self.showAlert(title : "1" , message : "2")
+        } , complationError : { code in
+            self.showAlert( title: "hay un error en " , message : "")
         })
+        
     }
+    
+    
+    func showAlert( title : String , message : String ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Entiendo", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+
 }
 
